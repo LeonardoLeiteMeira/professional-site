869b1ebb874a00a9b2b63fa3ecebb15b3f120e89
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2023-04-20 14:01:47 +0100

    fixed bug to update text when changed language

--- src/components/navigationArrow/index.tsx
+++ src/components/navigationArrow/index.tsx
@@ -1,5 +1,6 @@
 import { Grid, IconButton, Theme, Typography, useMediaQuery } from "@mui/material"
 import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import { useEffect, useState } from "react";
 
 interface Props{
     show:boolean
@@ -9,6 +10,12 @@ interface Props{
 }
 
 export default function NavigationArrow({show, text, arrowOrientation, onClick}:Props){
+    const [textState, setTextState] = useState(text)
+
+    useEffect(()=>{
+        setTextState(text)
+    },[text])
+
     const isDesktop = useMediaQuery((theme:Theme) => theme.breakpoints.up('md'));
 
     const isArrowUp = arrowOrientation==="up"
@@ -17,7 +24,7 @@ export default function NavigationArrow({show, text, arrowOrientation, onClick}:
         <Grid item container direction={"row"} wrap="nowrap" width={"100%"} justifyContent={"end"} alignItems={"center"}>
             {show?<></>:
                 <>
-                    <Typography variant={isDesktop?"body1":"body2"}>{text}</Typography>
+                    <Typography variant={isDesktop?"body1":"body2"}>{textState}</Typography>
                     <IconButton size="large" onClick={onClick}>
                         <ExpandCircleDownIcon sx={{transform:isArrowUp?'rotate(180deg)':"rotate(0deg)"}}/>
                     </IconButton>
