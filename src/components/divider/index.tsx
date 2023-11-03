f9cc2f87400221810ee23065c0063ee122c11204
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2023-11-03 17:32:50 +0000

    use only one design for divider

--- src/components/divider/index.tsx
+++ src/components/divider/index.tsx
@@ -3,17 +3,9 @@ import { styled } from "@mui/material";
 const DividerStyled = styled("div")(({ theme }) => ({
     width: "90vw",
     maxWidth:theme.spacing(40),
-    height: theme.spacing(0.25),
-    backgroundColor: `${theme.palette.secondary.main}`,
-    [theme.breakpoints.up("md")]:{
-        height: theme.spacing(1),
-        borderRadius:"20px"
-    },
-    [theme.breakpoints.down("md")]:{
-        height: theme.spacing(0.4),
-        borderRadius:"20px",
-        maxWidth:theme.spacing(25)
-    }
+    backgroundColor: `${theme.palette.secondary.main}`, 
+    height: theme.spacing(1),
+    borderRadius:"20px"
 }));
 
 export default function Divider(){
